97bd3ea9cac2d836895ddeb580d9f5e7627f2803
Augusto Garcia da Rosa <augusto.garciadarosa@gmail.com>
2021-03-18 14:22:36 -0300

    teste 1.9

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,24 @@
 const  knex = require('knex');
-var pg = require('pg');
-pg.defaults.ssl = 'true';
-const configuration = require('../../knexfile.js'); 
-const connection = knex(configuration.production);
+var parse = require('pg-connection-string').parse;
+const pgconfig = parse({
+    client:'pg',
+    connection: {
+      host:'ec2-52-71-161-140.compute-1.amazonaws.com',
+      database: 'd67focrh9lsj0f',
+      user:     'xpgqhrjnoerxnw',
+      password: 'passa0d51079126e32204ef339eca1fc46f95774dcd465a6476831bf1209c2df9509ord',
+      port: '5432',
+      ssl: true
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    }
+  });
+pgconfig.ssl = {rejectUnauthorized: false};
+//const configuration = require('../../knexfile.js'); 
+const connection = knex({
+    client: 'pg',
+    connection: pgconfig,
+});
+//const connection = knex(configuration.production);
 module.exports = connection;
