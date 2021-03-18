4371c25ac82b3d31ac8a4938112b67c847c3fc51
Augusto Garcia da Rosa <augusto.garciadarosa@gmail.com>
2021-03-18 14:04:09 -0300

    teste 1.7

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,4 +1,6 @@
 const  knex = require('knex');
+var pg = require('pg');
+pg.defaults.ssl = 'true';
 const configuration = require('../../knexfile.js'); 
 const connection = knex(configuration.production);
 module.exports = connection;
